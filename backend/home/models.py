924ae81264cd9da57678f0e4e7977967fb1b5d03
crowdbotics-dev <engineering@crowdbotics.com>
2023-02-27 05:48:32 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,13 @@
+from django.conf import settings
 from django.db import models
+class Breed(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=256,blank=True,)
+class Pet(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=200,)
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True,)
+    updated_at = models.DateTimeField(auto_now=True,)
+    birth_date = models.DateField()
+    breed = models.ForeignKey("home.Breed",on_delete=models.SET_NULL,null=True,blank=True,related_name="pet_breed",)
